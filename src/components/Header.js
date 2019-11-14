a265882a66c759823ea767c68d5d69d16b2ab84c
PiotrGasiorek <piotrjgasiorek@gmail.com>
2019-11-14 23:32:33 +0100

    Created folder for the components

---
+++ src/components/Header.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import styled from 'styled-components';
+import logo from '../gallery/logo.svg';
+import background from '../gallery/background.jpg';
+import Title from './Title';
+
+const Header = function(props) {
+    return (
+        <HeaderStyles>
+            <img src={logo} alt="App logo"/>
+            <div>
+                <Title>Start searching for photos</Title>
+
+            </div>
+        </HeaderStyles>
+    );
+}
+
+const HeaderStyles = styled.div `
+    width: 100%;
+    background: #007FCC;
+    height: 80vh;
+    position: relative;
+    background-image: url('${background}');
+    background-size: cover;
+    
+    img {
+        position: absolute;
+        top: 3rem;
+        left: 9vw;
+    }
+
+`;
+
+export default Header;
+
+
+
