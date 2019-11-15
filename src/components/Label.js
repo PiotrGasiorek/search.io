a6d0565459dad25f6356c24a60bd58967b84c997
PiotrGasiorek <piotrjgasiorek@gmail.com>
2019-11-15 11:31:05 +0100

    Created new component

---
+++ src/components/Label.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const Label = styled.label `
+    color: #969696;
+    font-size: 16px;
+    font-weight: 400;
+    transition: .5s;
+`;
+
+export default Label;
