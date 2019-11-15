8fa5849c7bbaaeba969688ecac03863affffa543
PiotrGasiorek <piotrjgasiorek@gmail.com>
2019-11-15 12:04:38 +0100

    Added state which change color of a label

--- src/components/Label.js
+++ src/components/Label.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 const Label = styled.label `
-    color: #969696;
+    color: ${props => props.active ? '#007FCC' : '#969696'};
     font-size: 16px;
     font-weight: 400;
     transition: .5s;
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,17 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react'
 import styled from 'styled-components';
+
+/* Components */
 import Input from './Input';
 import Button from './Button';
 import Label from './Label';
 
-const SearchForm = function(props) {
-    return (
-        <SearchFormStyles>
-            <Label>Keywords</Label>
-            <Input placeholder='Greece coast'/>
-            <Button>Search</Button>
-        </SearchFormStyles>
-    );
+class SearchForm extends Component {
+    constructor (props) {
+        super(props)
+        this.state = {
+          active: false
+        }
+  
+        this._onBlur = this._onBlur.bind(this)
+        this._onFocus = this._onFocus.bind(this)
+      }
+
+      _onBlur(){
+        console.log('not active');
+        this.setState({
+            active: false
+        })
+      }
+
+      _onFocus(){
+          console.log('active');
+        this.setState({
+            active: true
+        })
+      }
+
+    render() {
+        return (
+            <SearchFormStyles>
+                <Label active={this.state.active}>Keywords</Label>
+                <Input onFocus={this._onFocus} onBlur={this._onBlur} placeholder='Greece coast'/>
+                <Button>Search</Button>
+            </SearchFormStyles>
+        );
+    }
+    
 }
 
 
