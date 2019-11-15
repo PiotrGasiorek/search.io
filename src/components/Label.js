55dc204be6123f0854fe840304d43b23c889e07d
PiotrGasiorek <piotrjgasiorek@gmail.com>
2019-11-15 12:16:18 +0100

    Removed font weight style

--- src/components/Label.js
+++ src/components/Label.js
@@ -3,7 +3,6 @@ import styled from 'styled-components';
 const Label = styled.label `
     color: ${props => props.active ? '#007FCC' : '#969696'};
     font-size: 16px;
-    font-weight: 400;
     transition: .5s;
 `;
 
