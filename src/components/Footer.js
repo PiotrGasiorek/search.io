63cc77b5eca65e948e5ca602a8735c59ae062ecf
PiotrGasiorek <piotrjgasiorek@gmail.com>
2019-11-15 12:31:03 +0100

    Created new component

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+const Footer = styled.footer `
+    color: #969696;
+    font-size: 20px;
+    text-align: center;
+    margin: 4rem 0 3rem 0;
+
+    a {
+        color: #333333;
+        font-weight: 700;
+        text-decoration: none;
+    }
+`;
+
+export default Footer;
