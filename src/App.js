a7eda0a7f6ddcc865a4f6a48263f7a945f303147
PiotrGasiorek <piotrjgasiorek@gmail.com>
2019-11-15 12:32:23 +0100

    Implemented Footer component to the app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Footer>
+        <p>Created by <a href="https://piotrgasiorek.github.io/developer/">Piotr Gąsiorek</a></p>
+      </Footer>
     </div>  
   );
 }
