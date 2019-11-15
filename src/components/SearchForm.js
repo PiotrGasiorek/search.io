d71d95bedc05852040f078a6aa4337fad9749b38
PiotrGasiorek <piotrjgasiorek@gmail.com>
2019-11-15 11:33:16 +0100

    Imported all needed components Label, Input, Button

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,11 +1,28 @@
+import React from 'react';
 import styled from 'styled-components';
+import Input from './Input';
+import Button from './Button';
+import Label from './Label';
 
-const SearchForm = styled.form `
+const SearchForm = function(props) {
+    return (
+        <SearchFormStyles>
+            <Label>Keywords</Label>
+            <Input placeholder='Greece coast'/>
+            <Button>Search</Button>
+        </SearchFormStyles>
+    );
+}
+
+
+const SearchFormStyles = styled.form `
     max-width: 400px;
     background: white;
     border-radius: 3px;
     height: 250px;
     margin: 4vw auto;
+    padding: 30px 40px;
+    }
 `;
 
 export default SearchForm;
