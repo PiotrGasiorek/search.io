e284fdbbf0d43d5903ed27aa014bff0be75df697
PiotrGasiorek <piotrjgasiorek@gmail.com>
2019-11-23 16:29:58 +0100

    Changed font size on mobiles

--- src/components/Title.js
+++ src/components/Title.js
@@ -3,6 +3,10 @@ import styled from 'styled-components';
 const Title = styled.h1 `
     text-align: center;
     font-size: 40px;
+
+    @media only screen and (max-width: 500px) {
+      font-size: 30px;
+    }
 `;
 
 export default Title;
