705b0b73be5472c7174778bf259cc1b5c83bf64e
PiotrGasiorek <piotrjgasiorek@gmail.com>
2019-11-28 00:59:10 +0100

    Created component for notifications

---
+++ src/components/Notifications.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import styled from 'styled-components';
+import icon_alert from '../gallery/icon--alert.svg';
+
+export default function Notifications(){
+    return (
+        <NotificationsStyles id='notification'>
+            <img src={icon_alert} alt="Alert icon"/>
+            <p>We couldn't find anything</p>
+        </NotificationsStyles>
+    )
+} 
+
+const NotificationsStyles = styled.div `
+    padding: 10px 30px 10px 25px;
+    background: #007FCC;
+    display: none;
+    position: absolute;
+    right: 15px;
+    top: 15px;
+    box-sizing: border-box;
+    border-radius: 3px;
+
+    p{
+        margin-left: 15px;
+        font-weight: 500;
+        color: white;
+    }
+`;
