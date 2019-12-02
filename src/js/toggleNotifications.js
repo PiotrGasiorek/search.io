7da720ed126de1617f4a002e077e35b401d92a7b
PiotrGasiorek <piotrjgasiorek@gmail.com>
2019-12-02 16:42:41 +0100

    Added argument which enables calling varius informations

--- src/js/toggleNotifications.js
+++ src/js/toggleNotifications.js
@@ -1,6 +1,7 @@
-export default function toggleNotifications(){
+export default function toggleNotifications(information){
     let notification = document.getElementById('notification');
     if(!notification.classList.contains('showNotification')){
+        document.querySelector('#notification p').textContent = information;
         notification.classList.add('showNotification');
         setTimeout(() => {
             notification.classList.remove('showNotification');
